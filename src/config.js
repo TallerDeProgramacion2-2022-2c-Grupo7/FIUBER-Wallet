ff04a37b46007f44e2e08f789c8393b223e34210
0xAaCE <aacevedo@fi.uba.ar>
2022-12-04 10:46:03 -0300

    feat: add firebase (#1)
    * feat: update fastify and install firebase library
    
    * feat: implement and integrate auth middleware

--- src/config.js
+++ src/config.js
@@ -11,4 +11,9 @@ module.exports = {
   deployerMnemonic,
   infuraApiKey,
   network,
+  firebase: {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  },
 };
